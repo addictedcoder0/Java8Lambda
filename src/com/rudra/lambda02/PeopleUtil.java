793538e6617fab9d2ba5cb0e12858a74cf335824
abhishek <abhishek-2863@abhishek-2863.csez.zohocorpin.com>
2017-05-23 11:37:24 +0530

    completed 1st and 2nd task based on Lambda Exp

--- src/com/rudra/lambda02/PeopleUtil.java
+++ src/com/rudra/lambda02/PeopleUtil.java
@@ -20,16 +20,24 @@ public class PeopleUtil {
             return p1.getLastname().compareTo(p2.getLastname());
         });
 
-        System.out.println(" **** People sorted by lastName **** ");
-        printAll(people.getPeople());
+        System.out.println(" ********* People sorted by lastName ********* ");
+        printAll(people.getPeople(),(p)->true); // lambda based printing approach
 
         // 2. filter out all the people whose lastName starts with 'C' or 'c'
+        System.out.println("\n ********* People have lastName startWith \"C\" or \"c\" ******** ");
+        printAll(people.getPeople(),(p)->{
+            return p.getLastname().startsWith("C")||p.getLastname().startsWith("c");
+        });
+
+        // ( or ) we can reuse the generic interface provided by java which has the same signature as of our interface NameList
 
     }
 
-    static void printAll(List<Person> peopleList){
+    static void printAll(List<Person> peopleList,Namefilter condition){
         for(Person p : peopleList){
-            System.out.println(p);
+            if(condition.test(p)) {
+                System.out.println(p);
+            }
         }
     }
 }
